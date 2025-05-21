2284adb52449510d91f4f9e591d6ec74e1c45f74
SvetKos <153807994+SvetKos@users.noreply.github.com>
2025-05-21 22:31:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-SPREADSHEET_ID = "YOUR_SPREADSHEET_ID_HERE"
+BOT_TOKEN = "7708636195:AAFWF5RgXPBsSie-w-1bcVHfQsaaRadPubw"
+SPREADSHEET_ID = "https://docs.google.com/spreadsheets/d/1kF82CPAOw8Zu7vAlfDJYuwBJoOfWoATk-yVchEuGe24/edit?usp=sharing"
