5ed8f800b10474011db8d152cabbdc7eb2723943
SvetKos <153807994+SvetKos@users.noreply.github.com>
2025-05-29 21:28:06 +0300

    Update config29

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
-BOT_TOKEN = "7708636195:AAFWF5RgXPBsSie-w-1bcVHfQsaaRadPubw"
-SPREADSHEET_ID = "https://docs.google.com/spreadsheets/d/1kF82CPAOw8Zu7vAlfDJYuwBJoOfWoATk-yVchEuGe24/edit?usp=sharing"
+BOT_TOKEN = 7708636195:AAFWF5RgXPBsSie-w-1bcVHfQsaaRadPubw
+SPREADSHEET_ID = 1kF82CPAOw8Zu7vAlfDJYuwBJoOfWoATk-yVchEuGe24/edit?usp=sharing
